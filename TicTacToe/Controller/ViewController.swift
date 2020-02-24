97dbcdb5da0a85b2ef7adf1618d219580db35d56
Divyansh Srivastava <divyansh@mobiuso.com>
2020-02-24 14:52:54 +0530

    test

--- TicTacToe/Controller/ViewController.swift
+++ TicTacToe/Controller/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         let result = ticTacToeBoard.updateBoard(buttonTag: sender.tag)
         sender.setBackgroundImage(UIImage(named: ticTacToeBoard.updateBoardImage()), for: UIControl.State.normal)
         displayresult(flag: result)
-        sender.isEnabled = false
+        sender.isEnabled = true
     }
     
     @IBAction func restartPressed(_ sender: UIButton) {
